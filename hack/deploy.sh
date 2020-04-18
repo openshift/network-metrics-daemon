79f49e7255f3c9cdf19b23f46dd41519ceddb97c
Federico Paolinelli <fpaoline@redhat.com>
2020-04-18 08:48:29 +0200

    Pass the node name as a parameter instead of env variable.
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo $IMAGE_TAG
+# hack for having the node name variable not expanded in 03_daemonset.yaml
+export DOLLAR='$' 
 
 for file in $(ls -v deployments/); do
     echo "INFO - Applying file deployments/$file"
