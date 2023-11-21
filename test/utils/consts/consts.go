253d5ee983683156bd8f43371c2c308d78a5cf38
evgenLevin <elevin@redhat.com>
2023-11-21 14:42:11 -0500

    Added METRIC_TEST_IMAGE var

--- test/utils/consts/consts.go
+++ test/utils/consts/consts.go
@@ -1,5 +1,17 @@
 package consts
 
+import "os"
+
+// TestImage contains test image (default is quay.io/centos/centos)
+var TestImage string
+
+func init() {
+	TestImage = os.Getenv("METRIC_TEST_IMAGE")
+	if TestImage == "" {
+		TestImage = "quay.io/centos/centos"
+	}
+}
+
 const (
 	// TestingNamespace contains the name of the testing namespace
 	TestingNamespace = "metrictest"
