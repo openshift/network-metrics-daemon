26fabac3eb3f85f21bf549f95ac54a8d70a1ed19
Sebastian Sch <sebassch@gmail.com>
2024-06-19 11:45:26 +0300

    swtich golint install method
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- hack/get_tools.sh
+++ hack/get_tools.sh
@@ -3,4 +3,8 @@
 # Ensure that the tools needed to build locally are present
 set -xeuo pipefail
 
-GO111MODULE=off go get golang.org/x/lint/golint
+export CURPATH=`pwd`
+export BIN_DIR=$CURPATH/bin
+export GO111MODULE=on
+
+GOBIN=${BIN_DIR} go install -mod=mod golang.org/x/lint/golint@latest
